99efcd554703b1dfdd30e64a34b9192d5e081855
Jelson Araújo <jelsonmatheus200f@gmail.com>
2023-03-16 09:16:16 -0300

    feat: Add gitignore

--- automate_login.py
+++ automate_login.py
@@ -1,21 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
 
 # GranConcursos credenciais
-username = "usuario"
-password = "senha"
+USERNAME = ""
+PASSWORD = ""
 
-# inicializa o chrome drive
-driver = webdriver.Chrome(r"chromedriver")
-# entra na pagina do curso
-driver.get("https://www.grancursosonline.com.br/identificacao")
-# procura nome e insere no input
-driver.find_element("id", "login-email-site").send_keys(username)
-# procura senha e insere no input
-driver.find_element("id", "login-senha-site").send_keys(password)
-# clica no botão de login
-driver.find_element("id", "login-entrar-site").click()
-# aguarda a pagina ser carregada
-WebDriverWait(driver=driver, timeout=10).until(
-    lambda x: x.execute_script("return document.readyState === 'complete'")
-)
+
+def run():
+    # inicializa o chrome drive
+    chrome_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=chrome_options)
+
+    # entra na pagina do curso
+    driver.get("https://www.grancursosonline.com.br/identificacao")
+    # procura nome e insere no input
+    driver.find_element("id", "login-email-site").send_keys(USERNAME)
+    # procura senha e insere no input
+    driver.find_element("id", "login-senha-site").send_keys(PASSWORD)
+    # clica no botão de login
+    driver.find_element("id", "login-entrar-site").click()
+    # aguarda a pagina ser carregada
+    WebDriverWait(driver=driver, timeout=10).until(
+        lambda x: x.execute_script("return document.readyState === 'complete'")
+    )
+    time.sleep(20)
+
+if __name__ == '__main__':
+    run()
