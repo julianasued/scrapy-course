5f9f0eac53dfe6a2b4138d1e9e839339fa4f173a
Jelson Araújo <jelsonmatheus200f@gmail.com>
2023-03-19 00:43:57 -0300

    fix: download video

--- automate_login.py
+++ automate_login.py
@@ -5,35 +5,71 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from decouple import config
 import time
 import util
+import os
+
+
+SETTINGS = util.load_config()
 
 # GranConcursos credenciais
-USERNAME = config('LOGIN')
-PASSWORD = config('SENHA')
+USERNAME = SETTINGS['login']
+PASSWORD = SETTINGS['senha']
+LAST_REFRESH = time.time()
+
+
+def refresh_session(driver:'WebDriver', tab_id:'int'):
+    """
+    Atualiza a sessão do navegador
+    """
+    global LAST_REFRESH
+    current_time = time.time()
+
+    if((current_time - LAST_REFRESH) > 60):
+        driver.switch_to.window(driver.window_handles[0])
+        driver.refresh()
+        LAST_REFRESH = current_time
+        print("Sessão atualizada.")
+        util.wait_sleep(1, 1)
+        driver.switch_to.window(driver.window_handles[tab_id])
 
 
 def download_pdf(driver:'WebDriver', path:'Path'):
+    """
+    Faz o download de todos os PDFs de um concurso
+    """
+    print("-- Fazendo download dos pdf.")
     tab = driver.find_element(By.ID, "tab-row-pdf")
-    disciplinas = tab.find_elements(By.CSS_SELECTOR, "#accordion2 > div")
+    disciplinas = tab.find_elements(By.CSS_SELECTOR, "#accordion2.listagem-aulas > div")
     for disciplina in disciplinas:
         disciplina_nome = disciplina.find_element(By.CSS_SELECTOR, "h5 span").get_attribute("textContent")
+        aulas_target = disciplina.find_element(By.TAG_NAME, "div").get_attribute("data-target")
         disciplina_path = util.create_folder(path, disciplina_nome)
-        aulas = tab.find_elements(By.CSS_SELECTOR, "div[data-slug]")
+        aulas = disciplina.find_elements(By.CSS_SELECTOR, f"{aulas_target} > div")
         for aula in aulas:
             aula_nome = aula.find_element(By.CSS_SELECTOR, "h5 span").get_attribute("textContent") 
-            target = aula.get_attribute("data-target")[1:]
-            aula_path = util.create_folder(disciplina_path, f'pdf/{aula_nome}')
-            conteudos = disciplina.find_elements(By.CSS_SELECTOR, f'#{target} .card-body .item')
+            target = aula.find_element(By.TAG_NAME, "div").get_attribute("data-target")
+            aula_path = util.create_folder(disciplina_path, f'pdf{os.sep}{aula_nome.strip()}')
+            conteudos = aula.find_elements(By.CSS_SELECTOR, f'{target} .card-body .item')
             for conteudo in conteudos:
                 conteudo_nome = conteudo.find_element(By.CLASS_NAME, 'text-color-hover-blue-600').get_attribute("textContent")
-                pdf_url = conteudo.find_element(By.CSS_SELECTOR, 'a[aria-label="Baixar aula em PDF"]').get_attribute('href')
-                util.download_file(driver, pdf_url, aula_path, conteudo_nome)
-                util.wait_sleep(1, 2)
+                try:
+                    pdf_url = conteudo.find_element(By.CSS_SELECTOR, 'a[aria-label="Baixar aula em PDF"]').get_attribute("href")
+                    print("---- Baixando pdf: ", conteudo_nome)
+                    util.download_file(driver, pdf_url, aula_path, conteudo_nome)
+                    util.wait_sleep(2, 4)
+                except NoSuchElementException as err:
+                    print("Pdf não encontrado.")
+                refresh_session(driver, 1)
 
 
 def download_materiais(driver:'WebDriver', path:'Path'):
+    """
+    Faz o download de todos os materias de um 
+    """
+    print("-- Fazendo download dos materias.")
     tab_btn = driver.find_element(By.CSS_SELECTOR, 'a[href="#tab-row-materiais"]')
     driver.execute_script("arguments[0].click();", tab_btn) 
     util.wait_sleep(1, 2)
@@ -42,69 +78,124 @@ def download_materiais(driver:'WebDriver', path:'Path'):
     for material in materiais:
         material_nome = material.find_element(By.CSS_SELECTOR, ".lh-5").get_attribute("textContent")
         material_url = material.find_element(By.TAG_NAME, "a").get_attribute("href")
+        print("---- Baixando material: ", material_nome)
         util.download_file(driver, material_url, materiais_path, material_nome)
-        time.sleep(1)
+        util.wait_sleep(2, 4)
+        refresh_session(driver, 1)
 
 
-def download_videos(driver:'WebDriver', path:'Path'):
+def download_videos(driver:'WebDriver', path:'Path', links:dict):
+    """
+    Faz o download dos videos passados
+    """
+    nova_aba = True
+    for nome, link in sorted(links.items())[:1]:
+        print("---- Baixando video: ", nome)
+        if nova_aba:
+            driver.execute_script(f"window.open('{link}', '_blank')")
+            driver.switch_to.window(driver.window_handles[2])
+            nova_aba = False
+        else:
+            driver.get(link)
+            util.wait_sleep(5, 8)
+        video_url = driver.find_element(By.TAG_NAME, "video").get_attribute("src")
+        util.download_video(video_url, nome, path)
+        refresh_session(driver, 2)
+    driver.close()
+
+
+def download_video_aulas(driver:'WebDriver', path:'Path'):
+    """
+    Inicia o download das videos aulas
+    """
+    print("-- Fazendo download dos Videos.")
     tab = driver.find_element(By.ID, "tab-row-video")
     disciplinas = tab.find_elements(By.CSS_SELECTOR, "#accordion > div")
     for disciplina in disciplinas:
         disciplina_nome = disciplina.find_element(By.CSS_SELECTOR, "h5 span").get_attribute("textContent")
+        aulas_target = disciplina.find_element(By.TAG_NAME, "div").get_attribute("data-target")
         disciplina_path = util.create_folder(path, disciplina_nome)
-        aulas = tab.find_elements(By.CSS_SELECTOR, "div[data-slug]")
+        aulas = disciplina.find_elements(By.CSS_SELECTOR, f"{aulas_target} > div")
         for aula in aulas:
+            links_videos = {}
             aula_nome = aula.find_element(By.CSS_SELECTOR, "h5 span").get_attribute("textContent") 
-            target = aula.get_attribute("data-target")[1:]
-            aula_path = util.create_folder(disciplina_path, f'pdf/{aula_nome}')
-            conteudos = disciplina.find_elements(By.CSS_SELECTOR, f'#{target} .card-body .item')
+            aula_path = util.create_folder(disciplina_path, f'videos{os.sep}{aula_nome.strip()}')
+            conteudos = aula.find_elements(By.CSS_SELECTOR, f'.card-body .item')
             for conteudo in conteudos:
-                conteudo_nome = conteudo.find_element(By.CLASS_NAME, 'text-color-hover-blue-600').get_attribute("textContent")
-                videu_url = conteudo.find_element(By.CSS_SELECTOR, 'a[aria-label="Baixar aula em PDF"]').get_attribute('href')
-                #util.download_file(driver, pdf_url, aula_path, conteudo_nome)
-                #util.wait_sleep(1, 2)
-                print(videu_url)
+                a = conteudo.find_element(By.CSS_SELECTOR, "label a")
+                link = a.get_attribute("href")
+                nome = a.get_attribute("textContent")
+                links_videos[nome] = link
+                refresh_session(driver, 1)
 
+            download_videos(driver, aula_path, links_videos)
+            driver.switch_to.window(driver.window_handles[1])
+            util.wait_sleep(1, 2)
 
 def download_concurso(driver:'WebDriver', link:str, nome:str):
-    path = util.create_root_folder(nome)
-    driver.get(link)
+    """
+    Inicia download dos arquivos dos Concurso
+    """
+    print("# Fazendo download do concurso: ", nome)
+    path = util.create_root_folder(SETTINGS['root'], nome)
+    driver.execute_script(f"window.open('{link}', '_blank')")
+    driver.switch_to.window(driver.window_handles[1])
     util.wait_sleep(1, 3)
-    #download_pdf(driver, path)
-    #download_materiais(driver, path)
-    download_videos(driver, path)
+    download_video_aulas(driver, path)
+    download_pdf(driver, path)
+    download_materiais(driver, path)
+    driver.close()
 
+ 
 
-def download_concursos(driver:'WebDriver'):
-    driver.get("https://www.grancursosonline.com.br/aluno/espaco/meus-cursos")
-    util.wait_loading(driver)
+def download_concursos(driver:'WebDriver', pagina=1):
+    """
+    Inicia o download de todos os cursos matrículados
+    """
+    proxima_pagina = driver.find_elements(By.CSS_SELECTOR, "#pagination li.active + li.cursor-pointer")
     cursos = driver.find_elements(By.CSS_SELECTOR, '.card-curso')
-    
-    for curso in cursos:
-        nome = curso.find_element(By.CSS_SELECTOR, '[role="link"]').text
-        download_concurso(driver, curso.get_attribute("href"), nome)
-    time.sleep(5)
+    print("Pagina: ", pagina)
 
+    # Pecorre todas as páginas se 'pagina' igual a 0 Ou apenas a página Especificada
+    if SETTINGS['pagina'] == 0 or SETTINGS['pagina'] == pagina:
+        for curso in cursos:
+            nome = curso.find_element(By.CSS_SELECTOR, '[role="link"]').text
+            if len(SETTINGS['concursos']) == 0 or nome in SETTINGS['concursos']:
+                download_concurso(driver, curso.get_attribute("href"), nome)
+                driver.switch_to.window(driver.window_handles[0])
+                util.wait_sleep(1, 2)
+                refresh_session(driver, 0)
+                pass
+    if len(proxima_pagina) > 0:
+        print()
+        pagina_num = int(proxima_pagina[0].text)
+        driver.execute_script("arguments[0].click();", proxima_pagina[0])
+        util.wait_sleep(1, 1)
+        download_concursos(driver, pagina_num)
 
 
 def run():
     # inicializa o chrome drive
     chrome_options = webdriver.ChromeOptions()
+    if not SETTINGS['tela']:
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
 
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=chrome_options)
     driver.maximize_window()
 
     # entra na pagina do curso
     driver.get("https://www.grancursosonline.com.br/identificacao")
-    # procura nome e insere no input
     driver.find_element("id", "login-email-site").send_keys(USERNAME)
-    # procura senha e insere no input
     util.wait_sleep(1, 2)
     driver.find_element("id", "login-senha-site").send_keys(PASSWORD)
-    # clica no botão de login
     driver.find_element("id", "login-entrar-site").click()
-    # aguarda a pagina ser carregada
-    util.wait_sleep(5, 6)
+    LAST_REFRESH = time.time()
+    util.wait_sleep(2, 3)
+
+    driver.get("https://www.grancursosonline.com.br/aluno/espaco/meus-cursos")
+    util.wait_loading(driver)
     download_concursos(driver)
 
 
--- util.py
+++ util.py
@@ -1,11 +1,22 @@
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from pathlib import Path
 import random
 import time
 import requests
+import json
+import os
 
 
-def wait_loading(driver):
+def load_config():
+    f = open('configuracoes.json')
+    config = json.load(f)
+    f.close()
+    return config
+
+
+def wait_loading(driver:'WebDriver'):
     WebDriverWait(driver=driver, timeout=10).until(
         lambda x: x.execute_script("return document.readyState === 'complete'")
     )
@@ -15,12 +26,8 @@ def wait_sleep(a, b):
     time.sleep(random.randint(a, b))
 
 
-def create_root_folder(name:str):
-    names = name.split('-')
-    if len(names) > 1:
-        path = Path(f'data/{names[0]}/{names[1]}')
-    else:
-        path = Path(name)
+def create_root_folder(root:str, name:str):
+    path = Path(f'{root}{os.sep}{name}')
     path.mkdir(parents=True, exist_ok=True)
     return path
 
@@ -39,6 +46,22 @@ def download_file(driver:'WebDriver', url:str, path:'Path', name:str):
     s.headers.update(headers)
     s.cookies.update( {c['name']:c['value'] for c in driver.get_cookies()} )
     response = s.get(url)
+    name = name.replace('/', '-')
     file = open(path / f"{name.strip()}.pdf", 'wb')
     file.write(response.content)
     file.close()
+
+
+def download_video(url, name, path):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    }
+    s = requests.session()
+    s.headers.update(headers)
+    response = s.get(url)
+    name = name.replace('/', '-')
+    file = open(path / f"{name.strip()}.mp4", 'wb')
+    file.write(response.content)
+    file.close()
+
+    
